30f8b20500ab0f208c577d4d6de964208245decc
Alex <alex_llamas@icloud.com>
2019-07-11 12:00:29 -0700

    Added functionality for user to move something around on game screen

--- SpeedyShooter.py
+++ SpeedyShooter.py
@@ -1,1 +1,44 @@
-print ("hello world")
+import pygame
+pygame.init()
+
+
+#starter code simply sets up game widow and allows users to control a "character" with the arrow keys
+win = pygame.display.set_mode((500,500))
+
+pygame.display.set_caption("first game")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 15
+
+run = True
+
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+
+    if keys[pygame.K_RIGHT]:
+        x += vel
+
+    if keys[pygame.K_UP]:
+        y -= vel
+
+    if keys[pygame.K_DOWN]:
+        y += vel
+
+    win.fill((0,0,0))
+
+    pygame.draw.rect(win, (255, 0, 0), (x,y, width, height))
+    pygame.display.update()
+
+pygame.quit()
