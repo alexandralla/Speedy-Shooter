db11663a945389df25f74b47dade3b877e77a0f8
ejdeposit <edeposit@pdx.edu>
2019-07-22 17:25:56 -0700

    added collision detection and kill off coliding bugs

--- bugs.py
+++ bugs.py
@@ -26,24 +26,26 @@ def px_to_grid(length, pxPerUnit):
     return int(length/pxPerUnit)
 
 class Bug(pygame.sprite.Sprite):
-    def __init__(self, img, screenX, screenY, pxPerUnit):
+    def __init__(self, img, x, y, pxPerUnit):
+        #pygame.sprite.Sprite.init(self)
+        super().__init__()
         self.image = pygame.image.load(img)
-        self.screenX = screenX
-        self.screenY = screenY
+        self.x = x
+        self.y = y
         #grid functions maybe change this to calculate the objects grid location based on its center.
-        self.gridX = px_to_grid(self.screenX, pxPerUnit) 
-        self.gridY = px_to_grid(self.screenY, pxPerUnit)
+        self.gridX = px_to_grid(self.x, pxPerUnit) 
+        self.gridY = px_to_grid(self.y, pxPerUnit)
         self.rect = self.image.get_rect()
         
     def change_position(self, changeX, changeY, pxPerUnit):
-        self.screenX= self.screenX + changeX
-        self.screenY = self.screenY + changeY
-        self.gridX= px_to_grid(self.screenX, pxPerUnit) 
-        self.gridY= px_to_grid(self.screenY, pxPerUnit) 
+        self.x= self.x + changeX
+        self.y = self.y + changeY
+        self.gridX= px_to_grid(self.x, pxPerUnit) 
+        self.gridY= px_to_grid(self.y, pxPerUnit) 
 
     def update_rect(self):
-        self.rect.x=self.screenX
-        self.rect.y=self.screenY
+        self.rect.x=self.x
+        self.rect.y=self.y
 
 # -------------------------------------------------------
 #                        main 
@@ -76,43 +78,73 @@ DISPLAYSURF.fill(WHITE)
 #rect1= pygame.Rect(0, 0, 20, 20)
 #pygame.draw.rect(DISPLAYSURF, RED, rect1)
 
+#bug1 moves back and forth
 bug1=Bug('bug.png', 0, 0, pxPerUnit)
 bug1.update_rect()
-DISPLAYSURF.blit(bug1.image, (bug1.screenX, bug1.screenY))
+DISPLAYSURF.blit(bug1.image, (bug1.x, bug1.y))
 
-#make bug as game piece
+
+#bug2 moves down to colide with bug3
 bug2= Bug('bug.png', 50, 0, pxPerUnit)
 bug2.update_rect()
-DISPLAYSURF.blit(bug2.image, (bug2.screenX, bug2.screenY))
+DISPLAYSURF.blit(bug2.image, (bug2.x, bug2.y))
 
-#makle stationary bug to colide with
+#bug3 is stationary so that bug2 can hit it
 bug3= Bug('bug.png', 50, 200, pxPerUnit)
 bug3.update_rect()
-DISPLAYSURF.blit(bug3.image, (bug3.screenX, bug3.screenY))
+DISPLAYSURF.blit(bug3.image, (bug3.x, bug3.y))
 
-# main game loop
-bugDirection=1
+movingBugs=pygame.sprite.Group()
+movingBugs.add(bug2)
+
+stationaryBugs=pygame.sprite.Group()
+stationaryBugs.add(bug3)
+#count=0
+#for bug in stationaryBugs:
+    #count=count +1
+#print("stationary bug count", count)
+
+
+#had to initialize the bug direction so that bug 1 can move back and forth
+bugDirection=5
 while True:
     DISPLAYSURF.fill(BLACK)
     
     #move_rect(rect1, "down", 10)
     #pygame.draw.rect(DISPLAYSURF, RED, rect1) 
-    
-    
-    DISPLAYSURF.blit(bug1.image, (bug1.screenX, bug1.screenY))
+
+    #check for collisions from two sprite groups
+    #first argument and second is group, so might have to loop through sprites in one group to 
+    #to check for collisions for list of sprites in another group
+    #what setting does kill=True do?
+    #answer: removes them from group, but would need to draw bugs on screen by groups so they are 
+    #actually removed.
+    for bug in movingBugs:
+        bugOnBugCollisionList= pygame.sprite.spritecollide(bug, stationaryBugs, True)
+        if bugOnBugCollisionList:
+            movingBugs.remove(bug)    
+
+    DISPLAYSURF.blit(bug1.image, (bug1.x, bug1.y))
     #bug1.image.fill(WHITE)
-    if bug1.screenX > screenWidth:
-        bugDirection= -1
-    if bug1.screenX < 0:
-        bugDirection=1
-    bug1.screenX = bug1.screenX + bugDirection
+    if bug1.x >= screenWidth:
+        bugDirection= -5
+        print(bug1.x)
+        print(bug1.rect.right)
+    if bug1.x < 0:
+        bugDirection=5
+    bug1.x = bug1.x + bugDirection
     bug1.update_rect()
 
-    DISPLAYSURF.blit(bug2.image, (bug2.screenX, bug2.screenY))
-    bug2.change_position(0, 1, pxPerUnit)
-    bug2.update_rect()
 
-    DISPLAYSURF.blit(bug3.image, (bug3.screenX, bug3.screenY))
+    #draw moving bugs group
+    for bug in movingBugs:
+        DISPLAYSURF.blit(bug2.image, (bug2.x, bug2.y))
+        bug2.change_position(0, 1, pxPerUnit)
+        bug2.update_rect()
+
+    #dra stationary bugs group
+    for bug in stationaryBugs:
+        DISPLAYSURF.blit(bug.image, (bug.x, bug.y))
 
     for event in pygame.event.get():
         if event.type == QUIT:
